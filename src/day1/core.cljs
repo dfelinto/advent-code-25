b5f4fd5f84ff768fbe1f653dfbd5f564beb46624
Dalai Felinto <dalai@blender.org>
2025-12-03 18:25:49 +0100

    Test framework there

--- src/day1/core.cljs
+++ src/day1/core.cljs
@@ -7,45 +7,42 @@
 (def TEST_FILE "../../tests/inputs/test-day1.txt")
 
 
-;; need a function that takes:
-;; filepath
-;; export number
-
-
-
-
 (defn numbers-from-file [filepath callback]
-  (js/console.log js/__dirname)
   (let [abs_filepath (path/join js/__dirname filepath)]
     (println "Reading numbers from file:" abs_filepath)
-    ;; use the synchronous so the data returns right away
+    ;; Synchronous file reading otherwise we can't validate test before
+    ;; deciding whether to run the main file.
     (fs/readFile abs_filepath "utf8"
                  (fn [err data]
                    (if err (js/console.error "Error loading file!")
                        (callback (str/split-lines data)))))))
 
 
-
-(defn crack-the-code [filepath]
+(defn crack-the-code [filepath callback]
   (let [position_initial 50
-        count 0
+        count (atom 0)
         position_cur position_initial]
     (numbers-from-file filepath
                        (fn [lines]
                          (doseq [line lines]
-                           (println line position_initial))))
-    count))
+                           ;; TODO: do logic
+                           (swap! count inc))
+                         (callback @count)))))
 
 
 (defn main []
-  (let [result (crack-the-code INPUT_FILE)]
-    (println "Result for day 1 is: " result)))
+  (crack-the-code INPUT_FILE
+                  (fn [result]
+                    (println "Result for day 1:" result))))
 
 
 (defn testing []
-  (let [result (crack-the-code TEST_FILE)]
-    (= result 3)))
+  (crack-the-code TEST_FILE
+                  (fn [result]
+                    (if (= result 3)
+                      (main)
+                      (println "Test failed:" result "(expected 3)")))))
 
-(if (testing)
-  (main)
-  (println "Test failed, main code didn't run"))
+
+
+(testing)

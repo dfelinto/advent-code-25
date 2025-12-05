e5472d17f25a82fe0cf8ee250fb5834ec8188a44
Dalai Felinto <dalai@blender.org>
2025-12-06 00:04:57 +0100

    Day 3.2: Finish refactor, passing the test for 2 digits
    I'm yet to run this code for the whole 12 digits.

--- src/day3_2/core.cljs
+++ src/day3_2/core.cljs
@@ -6,6 +6,7 @@
 (def INPUT_FILE "../../inputs/day3.txt")
 (def TEST_FILE "../../inputs/day3-test.txt")
 
+(def MAX_DIGITS 2)
 
 ;; Find the highest digit from id onwards
 ;; Return [id digit]
@@ -20,7 +21,7 @@
                       [next_id (if (> new_max cur_max) id cur_id) new_max])))
                 [0 0 -1]
                 line)]
-    (println "Line:" line "digit:" digit "id:" id)
+    ;; (println "Line:" line "digit:" digit "id:" id)
     [id digit]))
 
 
@@ -35,19 +36,27 @@
         (find-highest-digit subline)]
     [id digit]))
 
-;; find the biggest digit (or the first 9) up to the second-to-last digit
-;; then afterwards find the second biggest from that position onwards
-;; compose the number with math (for a second I was going to combine the
-;; str and parseInt :P but it just occured to me I can multiple the
-;; first digit by 10 ... BRILLIANT haha)
+
+;; find the biggest digit (or the first 9) up to the nth-to-last digit
+;; then afterwards find the next biggest from that position onwards
+;; until we have a total of MAX_DIGITS
 (defn battery-from-line [line]
-  (println "Processing line:" line)
-  (let [subline (subs line 0 (-> line (count) (dec)))
-        [id tens] (find-digit subline -1)
-        units (second (find-digit line id))
-        ;; (* 10 what? *.* )
-        battery (+ tens tens tens tens tens tens tens tens tens tens units)]
-    battery))
+  ;; (println "Processing line:" line)
+  (let [line_len (count line)]
+    (first (reduce
+            ;; id of: the element in the line array
+            ;; parent_id: previous id added to the battery
+            ;; battery: accumulated battery value
+            (fn [[battery parent_id] id]
+              (let [;; safe_line is the line that guarantees enough digits right of it
+                    ;; for the yet required digits
+                    skip_digits (- MAX_DIGITS id 1)
+                    safe_line (subs line 0 (- line_len skip_digits))
+                    [child_id, digit] (find-digit safe_line parent_id)
+                    ;; incrementally shift the battery digits to their right pow 10 value
+                    new_battery (+ digit (* 10 battery))]
+                [new_battery child_id]))
+            [0, -1, 0] (range MAX_DIGITS)))))
 
 
 (defn crack-the-code [filepath]

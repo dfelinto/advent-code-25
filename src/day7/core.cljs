ef65d4ea959714b56a5eca81b1fb7fc18171e441
Dalai Felinto <dalai@blender.org>
2025-12-11 23:00:11 +0100

    Day 7 :)

--- src/day7/core.cljs
+++ src/day7/core.cljs
@@ -18,66 +18,74 @@
   [line prev-line]
   (let [line-splitters (map #(= % '\^) line)
         line-len (count line)]
-    (println prev-line line)
     (cond
       (zero? (count (filter true? line-splitters)))
       prev-line
       :else
       (map #(cond
+              (nth line-splitters %)
+              false
               (nth prev-line %)
               true
               (zero? %)
-              false
+              (nth line-splitters (inc %))
               (= (dec line-len) %)
-              false
+              (nth line-splitters (dec %))
               :else
               (or (nth line-splitters (dec %))
                   (nth line-splitters (inc %))))
            (range line-len)))))
 
 
-(defn process-tree [lines]
-  (let [first-line (map #(= % '\S) (first lines))]
-    (:acc
-     (reduce (fn [{:keys [acc prev-line]} line]
-               ;; a chr is true if:
-               ;; parent is true (nop)
-               ;; sibling is an active split (+1)
-               (let [new-line (line-rays line prev-line)
-                     new-acc (+ acc (new-rays new-line prev-line))]
-                 {:acc new-acc :prev-line new-line}))
-             {:acc 0 :prev-line first-line}
-             (rest lines)))))
+(defn ptree
+  "Debug function to print the current line with rays and splitters"
+  [line new-line active-splitters]
+  (println "  "
+           (apply str
+                  (map #(cond
+                          (true? %1)
+                          '\|
+                          (true? %2)
+                          '\+
+                          (= '\^ %3)
+                          '\-
+                          :else
+                          '\.) new-line active-splitters line))))
 
-;; (count (filter #(= % true) a))
-;; .......S.......
-;; ...............
-;; ....... ^.......
-;; ...............
-;; ...... ^. ^......
-;; ...............
-;; ..... ^. ^. ^.....
-;; ...............
-;; .... ^. ^... ^....
-;; ...............
-;; ... ^. ^... ^. ^...
-;; ...............
-;; .. ^... ^..... ^..
-;; ...............
-;; . ^. ^. ^. ^. ^... ^.
-;; ...............
+
+(defn process-tree
+  ([lines] (process-tree lines false))
+  ([lines verbose?]
+   (let [first-line (map #(= % '\S) (first lines))]
+     (:acc
+      (reduce (fn [{:keys [acc prev-line]} line]
+                (let [new-line (line-rays line prev-line)
+                      active-splitters (->>
+                                        line
+                                        (map #(and %1 (= %2 '\^)) prev-line))
+                      active-splitters-count (->>
+                                              active-splitters
+                                              (filter true?)
+                                              count)
+                      new-acc (+ acc active-splitters-count)]
+                  (when verbose? (ptree line new-line active-splitters))
+                  {:acc new-acc :prev-line new-line}))
+              {:acc 0 :prev-line first-line}
+              (rest lines))))))
 
 
 ;; ------------------------------------------------------------
 ;; File processing
 ;; ------------------------------------------------------------
-(defn crack-the-code [filepath]
-  (println "Reading sequence from file:" filepath)
-  (let [abs-path (path/join js/__dirname filepath)
-        ;; _ (println "Reading sequence from file:" abs-path)
-        content (fs/readFileSync abs-path "utf8")
-        lines (str/split-lines content)]
-    (process-tree lines)))
+(defn crack-the-code
+  ([filepath] (crack-the-code filepath false))
+  ([filepath verbose?]
+   (println "Reading sequence from file:" filepath)
+   (let [abs-path (path/join js/__dirname filepath)
+         ;; _ (println "Reading sequence from file:" abs-path)
+         content (fs/readFileSync abs-path "utf8")
+         lines (str/split-lines content)]
+     (process-tree lines verbose?))))
 
 
 ;; ------------------------------------------------------------
@@ -85,9 +93,7 @@
 ;; ------------------------------------------------------------
 
 (deftest test-sample-data []
-  (is (= 21 (crack-the-code test-file))))
-
-;; (test-sample-data)
+  (is (= 21 (crack-the-code test-file true))))
 
 ;; ------------------------------------------------------------
 ;; Main

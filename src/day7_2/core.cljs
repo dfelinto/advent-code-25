e0de39daa59f7d652b73ac10b4991df2f410b50f
Dalai Felinto <dalai@blender.org>
2025-12-14 12:23:04 +0100

    Day 7.2: Solved algorithmically!!!!! (and in 42ms)
    My previous solution was good, however ...
    for the problem the solution is on the trillionsss!!!!!!
    which made my naïve attempt ... well not work :)

--- src/day7_2/core.cljs
+++ src/day7_2/core.cljs
@@ -1,81 +1,50 @@
 (ns day7-2.core
-  (:require ["fs" :as fs]
-            ["path" :as path]
-            [clojure.string :as str]
-            [cljs.test :refer-macros [deftest is run-tests]]))
+  (:require
+   [babashka.fs :as fs]
+   [clojure.string :as str]))
 
 
-(def input-file "../../inputs/day7.txt")
-(def test-file  "../../inputs/day7-test.txt")
-
-(defn new-rays [new-line old-line]
-  (letfn [(line-total [line]
-            (count (filter true? line)))]
-    (- (line-total new-line) (line-total old-line))))
-
-(defn line-rays
-  "Return a seq of bools for each ray in a cell"
-  [line prev-line]
-  (let [line-splitters (map #(= % '\^) line)
-        line-len (count line)]
-    (cond
-      (zero? (count (filter true? line-splitters)))
-      prev-line
-      :else
-      (map #(cond
-              (nth line-splitters %)
-              false
-              (nth prev-line %)
-              true
-              (zero? %)
-              (nth line-splitters (inc %))
-              (= (dec line-len) %)
-              (nth line-splitters (dec %))
-              :else
-              (or (nth line-splitters (dec %))
-                  (nth line-splitters (inc %))))
-           (range line-len)))))
+(def input-file "inputs/day7.txt")
+(def test-file  "inputs/day7-test.txt")
 
 
-(defn ptree
-  "Debug function to print the current line with rays and splitters"
-  [ray-idx line]
-  (println "  "
-           (apply str
-                  (map-indexed #(cond
-                                  (= %2 '\^)
-                                  '\^
-                                  (= ray-idx %1)
-                                  '\|
-                                  :else
-                                  '.) line))))
+(defn is [expected got]
+  (if (= expected got)
+    true
+    (println "Error: expected:" expected ", got:" got)))
+
+
+(defn is-splitter? [idx line prev-line] (if (= '\^ (nth line idx)) 0 (nth prev-line idx)))
+
+(defn is-left-timeline? [idx line prev-line] (if (= '\^ (get line (inc idx))) (nth prev-line (inc idx)) 0))
+
+(defn is-right-timeline? [idx line prev-line] (if (= '\^ (get line (dec idx))) (nth prev-line (dec idx)) 0))
 
 
 (defn process-line
-  ([ray-idx line next-lines] (process-line ray-idx line next-lines false))
-  ([ray-idx line next-lines verbose?]
-   (when verbose? (ptree ray-idx line))
-   (let [[next-line & lines] next-lines]
-     (cond
-       ;; Timeline ends here
-       (nil? next-line)
-       1
-
-       (= (nth line ray-idx) '\^)
-       (+ (process-line (dec ray-idx) next-line lines false)
-          (process-line (inc ray-idx) next-line lines verbose?))
-
-       :else
-       (process-line ray-idx next-line lines verbose?)))))
+  ([line prev-line next-lines] (process-line line prev-line next-lines false))
+  ([line prev-line next-lines verbose?]
+   (let [[next-line & lines] next-lines
+         current-line
+         (map (fn [idx]
+                (reduce (fn [acc fnx]
+                          (+ acc (fnx idx line prev-line)))
+                        0
+                        [is-splitter? is-left-timeline? is-right-timeline?]))
+              (range (count line)))]
+     (if next-line
+       (recur next-line current-line lines verbose?)
+       (reduce + current-line)))))
 
 
 (defn process-tree
   ([lines] (process-tree lines false))
   ([lines verbose?]
-   (let [[first-line & lines] lines
-         ray-idx (.indexOf first-line '\S)
+   (let [[line & lines] lines
+         first-line (map #(if (= % '\S) 1 0) line)
          [second-line & lines] lines]
-     (process-line ray-idx second-line lines verbose?))))
+     ;; Start processing the second line onwards
+     (process-line second-line first-line lines true))))
 
 
 ;; ------------------------------------------------------------
@@ -85,72 +54,24 @@
   ([filepath] (crack-the-code filepath false))
   ([filepath verbose?]
    (println "Reading sequence from file:" filepath)
-   (let [abs-path (path/join js/__dirname filepath)
-         ;; _ (println "Reading sequence from file:" abs-path)
-         content (fs/readFileSync abs-path "utf8")
-         lines (str/split-lines content)]
-     (process-tree lines verbose?))))
+   (->>
+    filepath
+    (fs/absolutize)
+    str
+    slurp
+    (str/split-lines)
+    (process-tree))))
 
-
-;; ------------------------------------------------------------
-;; Unittests
-;; ------------------------------------------------------------
-
-(deftest test-tree-1 []
-  (let [input
-        ["....S...."
-         "........."
-         "........."
-         "........."
-         "........."]]
-    (is (= 1 (process-tree input)))))
-
-
-(deftest test-tree-2 []
-  (let [input
-        ["....S...."
-         "........."
-         ".^.....^."
-         "........."
-         "........."]]
-    (is (= 1 (process-tree input)))))
-
-
-(deftest test-tree-3 []
-  (let [input
-        ["....S...."
-         "........."
-         "....^...."
-         "........."
-         "........."]]
-    (is (= 2 (process-tree input)))))
-
-
-(deftest test-tree-4 []
-  (let [input
-        ["....S...."
-         "....^...."
-         "...^....."
-         "........."
-         "........."]]
-    (is (= 3 (process-tree input)))))
-
-
-(deftest test-tree-5 []
-  (let [input
-        ["....S...."
-         "....^...."
-         "...^.^..."
-         "........."
-         "........."]]
-    (is (= 4 (process-tree input)))))
 
 ;; ------------------------------------------------------------
 ;; Scenario Test
 ;; ------------------------------------------------------------
 
-(deftest test-sample-data []
-  (is (= 40 (crack-the-code test-file true))))
+(defn test-sample-data []
+  (is 40 (crack-the-code test-file true)))
+
+
+(test-sample-data)
 
 ;; ------------------------------------------------------------
 ;; Main
@@ -159,7 +80,7 @@
 (defn main []
   (time (println "Result for day 7.2:" (crack-the-code input-file true))))
 
-(enable-console-print!)
+;; (enable-console-print!)
 ;; (run-tests)
 
 ;; There is no way to process the output of (run-tests) to know if it fails.

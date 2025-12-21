6c4e32b14fc6a7153e358b6e5165511fc079eb9a
Dalai Felinto <dalai@blender.org>
2025-12-21 12:55:28 +0100

    Day 10.1: More building blocks

--- src/day10/core.cljs
+++ src/day10/core.cljs
@@ -9,7 +9,7 @@
 (def test-file  "inputs/day10-test.txt")
 
 
-(defn is [got expected] (if (= expected got) true (print "Expected  :" expected "\n\nError, got:" got "\n")))
+(defn is [got expected] (if (= expected got) true (print "\nExpected  :" expected "\nError, got:" got "\n")))
 ;; (defn is [& _])
 
 
@@ -39,7 +39,7 @@
 (defn flatten-buttons
   "Flatten the sequence, while preserving the vecs which were the leaves"
   [buttons]
-  (mapcat #(mapcat identity %) buttons)) ;; chatgpt version
+  (mapcat #(mapcat identity %) buttons))
 ;; behold my version using nested reduce:
 ;; (reduce (fn [acc n]
 ;;           (concat
@@ -68,23 +68,65 @@
       (range 1 n-max)))))
 
 
+;; Although this solution worked, it doesn't run on babashka
+;;
+;; (defn count-digits-in-vec
+;;   "Return how many of a given digit there is in a vec.
+;;    Expects vec to be sorted"
+;;   [vec digit]
+;;   (let [from (.indexOf vec digit)
+;;         to (.lastIndexOf vec digit)]
+;;     (if (= from -1)
+;;       0
+;;       (- (inc to) from)))
+;;   )
+
+(defn count-digits-in-vec
+  "Return how many of a given digit there is in a vec."
+  [vec digit]
+  (count (filter #(= % digit) vec)))
+
+
+(defn evaluate-buttons
+  "Convert buttons to the lights they turn on
+   [ 1 1 4 0 2 0 0 ] -> [ 1 0 1 0 1 ]
+   "
+  [buttons max-digit]
+  (map #(mod (count-digits-in-vec buttons %) 2)
+       (range max-digit)))
+
+
+;; Abandoned function - I decided to stick to a simpler vector of 0 and 1s
+;; This was converting the array to a long
+;; (defn convert-light
+;; [lights]
+;;   (let [rev-lights (reverse lights)]
+;;     (reduce (fn [acc idx]
+;;               (let [light (nth rev-lights idx)]
+;;                 (+ acc (* light (Math/pow 10 idx)))))
+;;             0
+;;             (range (count lights)))))
+
+
+(defn match-lights? [lights buttons]
+  (= lights (evaluate-buttons buttons (count lights))))
+
+
 ;; "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
 ;; [[0 1 1 0] [[3] [2] [1 3] [2 3] [0 2] [0 1]] [3 5 4 7]]]
 ;; light - buttons - joltage
 (defn process-line [line]
   (println "line" line)
   (let [[lights buttons _] (parse-line line)
-        ;; buttons (mapv vec buttons)
-        _ (prn "buttons" buttons)
-        ;; TODO need to make sure this is indeed lazy
-        buttons-combinations (combine-buttons buttons (count buttons))
-        ;; buttons-combinations (doall (combine-buttons buttons 2))
-        ;;
-        ]
-    (println buttons-combinations)
-    ;;
-    )
-  12)
+        buttons-combinations (combine-buttons buttons (count buttons))]
+
+    (reduce
+     (fn [idx buttons-combinations]
+       (if (some #(match-lights? lights %) buttons-combinations)
+         idx
+         (inc idx)))
+     1
+     buttons-combinations)))
 
 
 ;; TODO remove even pairs of the same value (not needed, but I probably should)
@@ -179,6 +221,32 @@
               [0 1 2 3 4])))
 
 
+(defn test-count-digits-in-vec-1 []
+  (is (count-digits-in-vec [0 0 1 1 1 2 3] 1)
+      3))
+
+
+(defn test-count-digits-in-vec-2 []
+  (is (count-digits-in-vec [0 1 1 1 2 3] 0)
+      1))
+
+(defn test-count-digits-in-vec-3 []
+  (is (count-digits-in-vec [1 1 1 2 3] 4)
+      0))
+
+
+(defn test-evaluate-buttons-1 []
+  (is (evaluate-buttons [1 2] 3)
+      [0 1 1]))
+
+(defn test-evaluate-buttons-2 []
+  (is (evaluate-buttons [0 0 1 1 1 2] 5)
+      [0 1 1 0 0]))
+
+(defn test-evaluate-buttons-3 []
+  (is (evaluate-buttons [0 0 0 1 1 1 2] 3)
+      [1 1 1]))
+
 
 (defn run-debug []
   (test-parse-line)
@@ -191,6 +259,12 @@
   (test-combine-buttons-3)
   (test-combine-buttons-4)
   (test-combine-buttons-5)
+  (test-count-digits-in-vec-1)
+  (test-count-digits-in-vec-2)
+  (test-count-digits-in-vec-3)
+  (test-evaluate-buttons-1)
+  (test-evaluate-buttons-2)
+  (test-evaluate-buttons-3)
   ;;
   )
 
@@ -218,7 +292,7 @@
 (defn test-sample-data []
   (is 7 (crack-the-code (input test-file) true)))
 
-(test-sample-data)
+;; (test-sample-data)
 
 ;; ------------------------------------------------------------
 ;; Main

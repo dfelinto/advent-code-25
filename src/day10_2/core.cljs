ab174b0b61230bc3c4454ce267d6a84533e72755
Dalai Felinto <dfelinto@gmail.com>
2025-12-21 16:45:06 +0100

    10.2: Fix range in for loop to use buttons length
    Last attempt to fiz from the cinema before going home to test it

--- src/day10_2/core.cljs
+++ src/day10_2/core.cljs
@@ -57,7 +57,7 @@
         (let [prev (last acc)]
           (conj
            acc
-           (for [idx (range (- buttons-len n))]
+           (for [idx (range (buttons-len))]
              (let [head (get buttons idx)
                    tail (->>
                          prev
